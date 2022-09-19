0adf128a201fa5150b8bb7ae8c70f75ce00774d4
Abhishek0090 <palabhishek411@gmail.com>
2022-09-19 18:24:18 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+  </head>
+    <title>birlaform</title>
+</head>
+<body>
+        <div class="container mt-5">
+            <div class="row col">
+                <form action="">
+
+                    <div class="form-group">
+                        <h3>DETAILS OF THE THIRD PARTY ADMINISTRATOR</h3><P>(To be filled in block letters)</P>
+                        <div class="col d-flex flex-row  ">
+                            
+                            <span class="alphabet">a.</span> Name of TPA/Insurance Company : Aditya Birla Insurance Company Limited 
+                        </div>
+                        <div class="col-md-6 aligninput">
+                        <span class="alphabet">b.</span>
+                    <label for="">Toll free phone number</label>
+                    <input type="text" class="form-control class">
+                     </div>
+                        <div class="col-md-6 aligninput">
+                        <span class="alphabet">c.</span>
+
+                    <label for="">Toll free Fax</label>
+                    <input type="text" class="form-control class">
+                </div>
+
+
+                <h3>TO BE FILLED BY THE INSURED/PATIENT</h3>
+                <div class="col-md-6">
+                <span class="alphabet">a.</span>
+
+                    <label for="">Name of the Patient</label>
+                    <input type="text" class="form-control class">
+                </div>
+                <div class="col-md-6">
+                    <label for="">Gender</label>
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+            <label class="form-check-label" for="inlineCheckbox1">1</label>
+            </div>
+            <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
+            <label class="form-check-label" for="inlineCheckbox2">2</label>
+            </div>
+                    <input type="text" class="form-control class">
+                </div>
+                    </div>
+                </form>
+                </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+</body>
+</html>
