38d983ca78116439e02ac030fafd7c2355c1c947
Abhishek0090 <palabhishek411@gmail.com>
2022-09-27 12:37:28 +0530

    final changes

--- admin.php
+++ admin.php
@@ -5,55 +5,56 @@
 
 
 //     // print_r($_POST);die;
-    $post_id =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-    $toll_number =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-    $toll_fax =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
+    $post_id =  !empty($_POST['post_id']) ? mysqli_real_escape_string($conn, $_POST['post_id']) : '';
+    $toll_number =  !empty($_POST['toll_number']) ? mysqli_real_escape_string($conn, $_POST['toll_number']) : '';
+    $toll_fax =  !empty($_POST['toll_fax']) ? mysqli_real_escape_string($conn, $_POST['toll_fax']) : '';
     
   
 //patient       
 $employeeid = !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$name_patient =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$gender =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$age = !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$dob =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$contact_patient =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$contact_relative =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$insured_number =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$policy_number =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$mediclaim =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$company_name =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$physician =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$name_physician =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$contact_physician =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
+$name_patient =  !empty($_POST['name_patient']) ? mysqli_real_escape_string($conn, $_POST['name_patient']) : '';
+$gender =  !empty($_POST['gender']) ? mysqli_real_escape_string($conn, $_POST['gender']) : '';
+$age = !empty($_POST['age']) ? mysqli_real_escape_string($conn, $_POST['age']) : '';
+$dob =  !empty($_POST['dob']) ? mysqli_real_escape_string($conn, $_POST['dob']) : '';
+$contact_patient =  !empty($_POST['contact_patient']) ? mysqli_real_escape_string($conn, $_POST['contact_patient']) : '';
+$contact_relative =  !empty($_POST['contact_relative']) ? mysqli_real_escape_string($conn, $_POST['contact_relative']) : '';
+$insured_number =  !empty($_POST['insured_number']) ? mysqli_real_escape_string($conn, $_POST['insured_number']) : '';
+$policy_number =  !empty($_POST['policy_number']) ? mysqli_real_escape_string($conn, $_POST['policy_number']) : '';
+$mediclaim =  !empty($_POST['mediclaim']) ? mysqli_real_escape_string($conn, $_POST['mediclaim']) : '';
+$company_name =  !empty($_POST['company_name']) ? mysqli_real_escape_string($conn, $_POST['company_name']) : '';
+$physician =  !empty($_POST['physician']) ? mysqli_real_escape_string($conn, $_POST['physician']) : '';
+$name_physician =  !empty($_POST['name_physician']) ? mysqli_real_escape_string($conn, $_POST['name_physician']) : '';
+$contact_physician =  !empty($_POST['contact_physician']) ? mysqli_real_escape_string($conn, $_POST['contact_physician']) : '';
 
 //doctor
-$name_doctor  =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$contact_doctor  =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$nature_illness  =  !empty($_POST['employeeid']) ? mysqli_real_escape_string($conn, $_POST['employeeid']) : '';
-$relevant  = mysqli_real_escape_string($conn, $_POST['relevant']);
-$duration  = mysqli_real_escape_string($conn, $_POST['duration']);
-$date_consultation  = mysqli_real_escape_string($conn, $_POST['date_consultation']);
-$past_history  = mysqli_real_escape_string($conn, $_POST['past_history']);
-$diagnosis  = mysqli_real_escape_string($conn, $_POST['diagnosis']);
-$icd_code  = mysqli_real_escape_string($conn, $_POST['icd_code']);
-$line_treatment  = mysqli_real_escape_string($conn, $_POST['line_treatment']);
-$invest_medical  = mysqli_real_escape_string($conn, $_POST['invest_medical']);
-$route_drug  = mysqli_real_escape_string($conn, $_POST['route_drug']);
-$name_surgery  = mysqli_real_escape_string($conn, $_POST['name_surgery']);
-$icd_pcs  = mysqli_real_escape_string($conn, $_POST['icd_pcs']);
-$other_treatment  = mysqli_real_escape_string($conn, $_POST['other_treatment']);
-$injury_occur  = mysqli_real_escape_string($conn, $_POST['injury_occur']);
+$name_doctor  =  !empty($_POST['name_doctor']) ? mysqli_real_escape_string($conn, $_POST['name_doctor']) : '';
+$contact_doctor  =  !empty($_POST['contact_doctor']) ? mysqli_real_escape_string($conn, $_POST['contact_doctor']) : '';
+$nature_illness  =  !empty($_POST['nature_illness']) ? mysqli_real_escape_string($conn, $_POST['nature_illness']) : '';
+$relevant  =  !empty($_POST['relevant']) ? mysqli_real_escape_string($conn, $_POST['relevant']) : '';
+$duration  =  !empty($_POST['duration']) ? mysqli_real_escape_string($conn, $_POST['duration']) : '';
+$date_consultation  =  !empty($_POST['date_consultation']) ? mysqli_real_escape_string($conn, $_POST['date_consultation']) : '';
+$past_history  =  !empty($_POST['past_history']) ? mysqli_real_escape_string($conn, $_POST['past_history']) : '';
+$diagnosis  =  !empty($_POST['diagnosis']) ? mysqli_real_escape_string($conn, $_POST['diagnosis']) : '';
+$icd_code  =  !empty($_POST['icd_code']) ? mysqli_real_escape_string($conn, $_POST['icd_code']) : '';
+$line_treatment  =  !empty($_POST['line_treatment']) ? mysqli_real_escape_string($conn, $_POST['line_treatment']) : '';
+$invest_medical  =  !empty($_POST['invest_medical']) ? mysqli_real_escape_string($conn, $_POST['invest_medical']) : '';
+$route_drug  =  !empty($_POST['route_drug']) ? mysqli_real_escape_string($conn, $_POST['route_drug']) : '';
+$name_surgery  =  !empty($_POST['name_surgery']) ? mysqli_real_escape_string($conn, $_POST['name_surgery']) : '';
+$icd_pcs  =  !empty($_POST['icd_pcs']) ? mysqli_real_escape_string($conn, $_POST['icd_pcs']) : '';
+$other_treatment  =  !empty($_POST['other_treatment']) ? mysqli_real_escape_string($conn, $_POST['other_treatment']) : '';
+$injury_occur  =  !empty($_POST['injury_occur']) ? mysqli_real_escape_string($conn, $_POST['injury_occur']) : '';
 
     if($_POST['post_id'] != ''){
 
         //updating data
         $sql = "UPDATE  admin SET toll_number='".$toll_number."' , toll_fax='".$toll_fax."' , name_patient = '".$name_patient."', gender = '".$gender."' , age = '".$age."' , dob = '".$dob."' , contact_patient = '".$contact_patient."' , contact_relative = '".$contact_relative."' , insured_number = '".$insured_number."' , policy_number = '".$policy_number."' , mediclaim = '".$mediclaim."' , company_name = '".$company_name."' ,  physician = '".$physician."' , name_physician = '".$name_physician."' , contact_physician = '".$contact_physician."' , name_doctor = '".$name_doctor."' , contact_doctor = '".$contact_doctor."' , nature_illness = '".$nature_illness."' , relevant = '".$relevant."' , duration = '".$duration."' , date_consultation = '".$date_consultation."' , past_history = '".$past_history."' , diagnosis = '".$diagnosis."' , icd_code = '".$icd_code."', line_treatment = '".$line_treatment."' , invest_medical = '".$invest_medical."' , route_drug = '".$route_drug."' , name_surgery = '".$name_surgery."' , icd_pcs = '".$icd_pcs."' , other_treatment = '".$other_treatment."' , injury_occur = '".$injury_occur."'    where id = '".$_POST['post_id']."'";
         mysqli_query($conn,$sql);
+        echo $_POST['post_id'];
         
         
     }
     else{
-        print_r($_POST); die;
+        // print_r($_POST['age']); die;
         //inserting data
 $sql = "INSERT INTO admin(toll_number,toll_fax,employeeid,name_patient,gender,age,dob,contact_patient,contact_relative,insured_number,policy_number,mediclaim,company_name,physician,name_physician,contact_physician,name_doctor,contact_doctor,nature_illness,relevant,duration,date_consultation,past_history,diagnosis,icd_code,line_treatment,invest_medical,route_drug,name_surgery,icd_pcs,other_treatment,injury_occur) VALUES('$toll_number','$toll_fax','$employeeid','$name_patient','$gender','$age','$dob','$contact_patient','$contact_relative','$insured_number','$policy_number','$mediclaim','$company_name','$physician','$name_physician','$contact_physician','$name_doctor','$contact_doctor','$nature_illness','$relevant','$duration','$date_consultation','$past_history','$diagnosis','$icd_code','$line_treatment','$invest_medical','$route_drug','$name_surgery','$icd_pcs','$other_treatment','$injury_occur')";
         mysqli_query($conn,$sql);
--- index.php
+++ index.php
@@ -57,18 +57,18 @@
                 <span class="alphabet">a.</span>
 
                     <label for="">Name of the Patient</label>
-                    <input type="text" class="form-control class" name="name_patient"  id="name_patient">
+                    <input type="text" class="form-control class autosave" name="name_patient"  id="name_patient">
                 </div>
                 <div class="col-md-6 aligninput">
                 <span class="alphabet">b.</span>
                     <label for="">Gender</label>
             
                         <div class="form-check checkwidth">
-            <input  type="radio" class="form-check-input"   id="radio1" name="gender" value="Male" checked >Male
+            <input  type="radio" class="form-check-input autosave"   id="radio1" name="gender" value="Male" checked >Male
             <label class="form-check-label" for="radio1"></label>
             </div>
             <div class="form-check">
-            <input type="radio" class="form-check-input" id="radio2" name="gender" value="Female" >Female
+            <input type="radio" class="form-check-input autosave" id="radio2" name="gender" value="Female" >Female
             <label class="form-check-label" for="radio2" ></label>
             </div>
                         </div>
@@ -80,7 +80,7 @@
                     <div class="age aligninput">
                         
                         <label for="">Age</label>
-                        <input type="text" class="form-control class" id="age" name="age" > 
+                        <input type="text" class="form-control autosave" id="age" name="age" > 
                         
                     </div>
                 </div>
@@ -89,42 +89,42 @@
                 <span class="alphabet">d.</span>
 
                     <label for="">Date of Birth</label>
-                    <input type="text" class="form-control class" id="dob"  name="dob" >
+                    <input type="text" class="form-control autosave" id="dob"  name="dob" >
                 </div>
 
                 <div class="col-md-6 aligninput">
                 <span class="alphabet">e.</span>
 
                     <label for="">Contact Number</label>
-                    <input type="text" class="form-control class" id="contact_patient" name="contact_patient" >
+                    <input type="text" class="form-control autosave" id="contact_patient" name="contact_patient" >
                 </div>
 
                 <div class="col-md-6 aligninput">
                 <span class="alphabet">f.</span>
 
                     <label for="">Contact number of attending relative</label>
-                    <input type="text" class="form-control class" id="contact_relative" name="contact_relative" >
+                    <input type="text" class="form-control autosave" id="contact_relative" name="contact_relative" >
                 </div>
 
                 <div class="col-md-6 aligninput">
                 <span class="alphabet">g.</span>
 
                     <label for="">Insured Card ID number</label>
-                    <input type="text" class="form-control class" id="insured_number" name="insured_number" >
+                    <input type="text" class="form-control autosave" id="insured_number" name="insured_number" >
                 </div>
 
                 <div class="col-md-6 aligninput">
                 <span class="alphabet">h.</span>
 
                     <label for="">Policy number / Name of corporate</label>
-                    <input type="text" class="form-control class" id="policy_number" name="policy_number" ">
+                    <input type="text" class="form-control autosave" id="policy_number" name="policy_number" ">
                 </div>
 
                 <div class="col-md-6 aligninput">
                 <span class="alphabet">i.</span>
 
                     <label for="">Employee ID</label>
-                    <input type="text" class="form-control class" id="employeeid" name="employeeid" >
+                    <input type="text" class="form-control autosave" id="employeeid" name="employeeid" >
                 </div>
 
                 <div class="col-md-6 aligninput">
@@ -132,11 +132,11 @@
 
                     <label for="">Currently do you have any other Mediclaim/Health insurance</label>
                     <div class="form-check checkwidth">
-            <input  type="radio" class="form-check-input"   id="radio1" name="mediclaim" value="Yes" checked >Yes
+            <input  type="radio" class="form-check-input autosave"   id="radio1" name="mediclaim" value="Yes" checked >Yes
             <label class="form-check-label" for="radio1"></label>
             </div>
             <div class="form-check">
-            <input type="radio" class="form-check-input" id="radio2" name="mediclaim" value="No" >No
+            <input type="radio" class="form-check-input autosave" id="radio2" name="mediclaim" value="No" >No
             <label class="form-check-label" for="radio2" ></label>
             </div>
 
@@ -146,18 +146,18 @@
 
                     <label for="">Company Name:</label>
                     <label for="">Give details</label>
-                    <input type="text" class="form-control class" id="company_name" name="company_name" >
+                    <input type="text" class="form-control autosave" id="company_name" name="company_name" >
                 </div>
                 <div class="col-md-6 aligninput">
                 <span class="alphabet">l.</span>
 
                     <label for="">Do you have any family  physician</label>
                     <div class="form-check checkwidth">
-            <input  type="radio" class="form-check-input"   id="radio1" name="physician" value="Yes" checked >Yes
+            <input  type="radio" class="form-check-input autosave"   id="radio1" name="physician" value="Yes" checked >Yes
             <label class="form-check-label" for="radio1"></label>
             </div>
             <div class="form-check">
-            <input type="radio" class="form-check-input" id="radio2" name="physician" value="No" >No
+            <input type="radio" class="form-check-input autosave" id="radio2" name="physician" value="No" >No
             <label class="form-check-label" for="radio2" ></label>
             </div>
             </div>
@@ -166,13 +166,13 @@
                 <span class="alphabet">m.</span>
 
                     <label for="">Name of the family physician</label>
-                    <input type="text" class="form-control class" id="name_physician" name="name_physician" >
+                    <input type="text" class="form-control autosave" id="name_physician" name="name_physician" >
                 </div>
                 <div class="col-md-6 aligninput">
                 <span class="alphabet">n.</span>
 
                     <label for="">Contact number if any :</label>
-                    <input type="text" class="form-control class" id="contact_physician" name="contact_physician" >
+                    <input type="text" class="form-control autosave" id="contact_physician" name="contact_physician" >
                     </div>
                     
                 
@@ -186,25 +186,25 @@
                         
                         <label for="">Name of the treating doctor :</label>
                 
-                        <input type="text" class="form-control class" id="name_doctor" name="name_doctor">
+                        <input type="text" class="form-control autosave" id="name_doctor" name="name_doctor">
                     </div> 
                     <div class="col-md-6 aligninput">
                         <span class="alphabet">b.</span>
                         
                         <label for="">Contact number:</label>
-                        <input type="text" class="form-control class" id="contact_doctor" name="contact_doctor">
+                        <input type="text" class="form-control autosave" id="contact_doctor" name="contact_doctor">
                 </div> 
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">c.</span>
                     
                     <label for="">Name of ILLNESS / Disease with presenting Complaints :</label>
-                    <input type="text" class="form-control class" id="nature_illness" name="nature_illness" >
+                    <input type="text" class="form-control autosave" id="nature_illness" name="nature_illness" >
                 </div> 
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">d.</span>
                     
                     <label for="">Relevant clinical findings :</label>
-                    <input type="text" class="form-control class" id="relevant" name="relevant" >
+                    <input type="text" class="form-control autosave" id="relevant" name="relevant" >
                 </div>
                
                 <div class="col-md-6 aligninput">
@@ -216,66 +216,66 @@
 </div>
 <div class="col-md-6 alighinput d-flex">
                     <label for="">Date of first consultation :</label>
-                    <input type="text" class="form-control class" id="date_consultation" name="date_consultation" style="min-width:16rem ; margin-right:10px"; >
+                    <input type="text" class="form-control autosave" id="date_consultation" name="date_consultation" style="min-width:16rem ; margin-right:10px"; >
                         
                     <label for="">Past history of present ailment if any :</label>
-                    <input type="text" class="form-control class" id="past_history" name="past_history" style="min-width:16rem";>
+                    <input type="text" class="form-control autosave" id="past_history" name="past_history" style="min-width:16rem";>
 
                 </div>
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">f.</span>
                     
                     <label for="">Provisional diagnosis :</label>
-                    <input type="text" class="form-control class" id="diagnosis" name="diagnosis">
+                    <input type="text" class="form-control autosave" id="diagnosis" name="diagnosis">
                 </div>
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">g.</span>
                     
                     <label for="">ICD 10 Code :</label>
-                    <input type="text" class="form-control class" id="icd_code" name="icd_code" >
+                    <input type="text" class="form-control autosave" id="icd_code" name="icd_code" >
                 </div>
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">h.</span>
                     
                     <label for="" style="margin-right:12px";>Proposed line of treatment :</label>
                     <label for="" style="margin:auto ; margin-right:10px;">Medical Management Surgical Management Intensive care Investigation Non allopathic treatment.</label>
-                    <input type="text" class="form-control class" id="line_treatment" name="line_treatment" >
+                    <input type="text" class="form-control autosave" id="line_treatment" name="line_treatment" >
                 </div>
                 <div class="col-md-6 aligninput" >
                     <span class="alphabet">I.</span>
                     
                     <label for="">If Investigation &/or Medical Management provide :</label>
-                    <input type="text" class="form-control class" id="invest_medical" name="invest_medical" >
+                    <input type="text" class="form-control autosave" id="invest_medical" name="invest_medical" >
                 </div>
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">j.</span>
                     
                     <label for="">Route of drug administartion :</label>
-                    <input type="text" class="form-control class" id="route_drug" name="route_drug" >
+                    <input type="text" class="form-control autosave" id="route_drug" name="route_drug" >
                 </div>
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">k.</span>
                     
                     <label for="">If Surgical, name of surgery :</label>
-                    <input type="text" class="form-control class" id="name_surgery" name="name_surgery" > 
+                    <input type="text" class="form-control autosave" id="name_surgery" name="name_surgery" > 
                 </div>
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">l.</span>
                     
                     <label for="">ICD 10 PCS Code :</label>
-                    <input type="text" class="form-control class" id="icd_pcs" name="icd_pcs" >
+                    <input type="text" class="form-control autosave" id="icd_pcs" name="icd_pcs" >
                 </div>
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">m.</span>
                     
                     <label for="">If other treatments provide details :</label>
-                    <input type="text" class="form-control class" id="other_treatment" name="other_treatment" >
+                    <input type="text" class="form-control autosave" id="other_treatment" name="other_treatment" >
                 </div>
                 <div class="col-md-6 aligninput">
                     <span class="alphabet">n.</span>
                     
                     <label for="">How did injury occur :</label>
-                    <input type="text" class="form-control class" id="injury_occur" name="injury_occur" >
+                    <input type="text" class="form-control autosave" id="injury_occur" name="injury_occur" >
                 </div>
               
             
@@ -298,8 +298,8 @@
 <script>
   
 $(document).ready(function () {
-      $('.autosave').change(function (e) { 
-        e.preventDefault();
+      $('.autosave').change(function () { 
+        // e.preventDefault();
     
            var toll_number = $('#toll_number').val();  
         //    alert(toll_number);return false;
